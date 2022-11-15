a231732a0940906601f0b388ea513d91ac927a7e
Parthib Sarkar <mail4parthib@gmail.com>
2022-11-15 11:58:35 +0530

    Delete Database_First_Approach directory

--- Database_First_Approach/DatabaseFirstAppPK/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DatabaseFirstAppPK.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
