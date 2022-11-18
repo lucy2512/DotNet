28397abbea87f66ff64248c572b99258083c0572
lucy2512 <mail4parthib@gmail.com>
2022-11-18 11:58:12 +0530

    Initial Commit

---
+++ EMP_SAL_CALCULATOR/EMP_SAL_CALCULATOR/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EMP_SAL_CALCULATOR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
