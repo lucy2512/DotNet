d40671652ca100045f117bbf98005dc677ee7de7
lucy2512 <mail4parthib@gmail.com>
2022-11-18 12:01:53 +0530

    Salary Details Model Added

---
+++ EMP_SAL_CALCULATOR/EMP_SAL_CALCULATOR/Models/SalDet.cs
@@ -0,0 +1,11 @@
+﻿namespace EMP_SAL_CALCULATOR.Models
+{
+    public class SalDet
+    {
+        public string? EmpName { get; set; }
+        public float Salary { get; set; }
+        public float Tax { get; set; }
+        public float AccSalary { get; set; }
+
+    }
+}
