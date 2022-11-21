dc2453e7a66f7ec24290381c0287393ede10308f
lucy2512 <mail4parthib@gmail.com>
2022-11-21 17:22:07 +0530

    MINI PROJECT

---
+++ Event Manager/College Space/Data/College_SpaceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using College_Space.Models;
+
+namespace College_Space.Data
+{
+    public class College_SpaceContext : DbContext
+    {
+        public College_SpaceContext (DbContextOptions<College_SpaceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<College_Space.Models.Event> Events { get; set; } = default!;
+    }
+}
---
+++ Event Manager/College Space/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace College_Space.Models
+{
+    public class User
+    {
+        
+        public string? UserName { get; set; }
+        public string? PassWord { get; set; }
+    }
+}
