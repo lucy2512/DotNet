6eebddc1c2e2963493cdf27b099165b4beae3746
Parthib Sarkar <mail4parthib@gmail.com>
2022-11-21 17:16:09 +0530

    Delete Event Manager directory

--- Event Manager/College Space/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace College_Space.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
